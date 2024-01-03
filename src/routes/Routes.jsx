14c702b4ff2f79065a1f7014dfaf820ecca2371f
Khuong Mai (Lenovo SLim7 ProX) <maikhuong2003@gmail.com>
2024-01-03 15:42:49 +0700

    add more data & add teams page

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -2,14 +2,15 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import HomePage from '../pages/home/HomePage';
-import RankingBoard from '../components/RankingBoard';
+import RankingBoard from '../pages/ranking/RankingBoardPage';
+import { TeamPage } from '../pages/team/TeamPage';
 
 const MainRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/ranking" element={<RankingBoard />} />
-      {/* <Route path="/profile" element={<Profile />} /> */}
+      <Route path="/teams" element={<TeamPage />} />
       {/* Add more routes as needed */}
     </Routes>
   );
