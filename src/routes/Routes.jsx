7e15be3df30b04f92f04647035575ae27887e83e
Vo Gia Khang <khang.vogia@di4l.vn>
2024-01-04 15:39:32 +0700

    create player statistics page

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,12 +1,10 @@
-// Routes.jsx
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import HomePage from '../pages/home/HomePage';
-import RulePage from '../pages/rule/RulePage';
-import RankingBoard from '../pages/ranking/RankingBoardPage';
-import { TeamPage } from '../pages/team/TeamPage';
-import PlayerDetails from '../pages/player/PlayerDetail';
-
+import { Routes, Route } from "react-router-dom";
+import HomePage from "../pages/home/HomePage";
+import RulePage from "../pages/rule/RulePage";
+import RankingBoard from "../pages/ranking/RankingBoardPage";
+import { TeamPage } from "../pages/team/TeamPage";
+import PlayerDetails from "../pages/player/PlayerDetail";
+import PlayerStatistics from "../pages/playerStatistics/playerStatistics";
 
 const MainRoutes = () => {
   return (
@@ -17,6 +15,8 @@ const MainRoutes = () => {
       {/* <Route path="/profile" element={<Profile />} /> */}
       <Route path="/teams" element={<TeamPage />} />
       <Route path="/player/:id/details" element={<PlayerDetails />}></Route>
+      <Route path="/player-statistics" element={<PlayerStatistics />}></Route>
+
       {/* Add more routes as needed */}
     </Routes>
   );
